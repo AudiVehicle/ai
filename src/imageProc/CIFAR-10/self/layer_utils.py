ee719bb7d9f1b7c743b630692a3f523443eeb3b7
wangquanzhou <audi.car@qq.com>
2020-12-15 10:56:52 +0800

    add comment

--- src/imageProc/CIFAR-10/self/layer_utils.py
+++ src/imageProc/CIFAR-10/self/layer_utils.py
@@ -31,6 +31,19 @@ def ReLu(x):
 
 
 def softmax_loss(x, y):
+    """
+    Computes the loss and gradient for softmax classification.
+
+    Inputs:
+    - x: Input data, of shape (N, C) where x[i, j] is the score for the jth class
+      for the ith input.
+    - y: Vector of labels, of shape (N,) where y[i] is the label for x[i] and
+      0 <= y[i] < C
+
+    Returns a tuple of:
+    - loss: Scalar giving the loss
+    - dx: Gradient of the loss with respect to x
+    """
     probs = np.exp(x - np.max(x, axis=1, keepdims=True))
     probs /= np.sum(probs, axis=1, keepdims=True)
     N = x.shape[0]
