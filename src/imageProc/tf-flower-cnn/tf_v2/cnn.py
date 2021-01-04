1360d3fab869619869dd9ed66235711ec69f56bb
wangquanzhou <audi.car@qq.com>
2021-01-04 20:59:36 +0800

    add note

--- src/imageProc/tf-flower-cnn/tf_v2/cnn.py
+++ src/imageProc/tf-flower-cnn/tf_v2/cnn.py
@@ -3,6 +3,7 @@ import numpy as np
 import os
 import PIL
 import tensorflow as tf
+import time
 
 ## https://tensorflow.google.cn/tutorials/images/classification
 ## https://github.com/tensorflow/docs/blob/master/site/en/tutorials/images/classification.ipynb
@@ -13,7 +14,7 @@ from tensorflow.keras import layers
 from tensorflow.keras.models import Sequential
 import pathlib
 
-path='/Users/wangquanzhou/IdeaProjects/ai/dataset/flower_photos'
+path = '/Users/wangquanzhou/IdeaProjects/ai/dataset/flower_photos'
 
 data_dir = pathlib.Path(path)
 
@@ -35,37 +36,36 @@ img_height = 180
 img_width = 180
 
 train_ds = tf.keras.preprocessing.image_dataset_from_directory(
-  data_dir,
-  validation_split=0.2,
-  subset="training",
-  seed=123,
-  image_size=(img_height, img_width),
-  batch_size=batch_size)
+    data_dir,
+    validation_split=0.2,
+    subset="training",
+    seed=123,
+    image_size=(img_height, img_width),
+    batch_size=batch_size)
 
 val_ds = tf.keras.preprocessing.image_dataset_from_directory(
-  data_dir,
-  validation_split=0.2,
-  subset="validation",
-  seed=123,
-  image_size=(img_height, img_width),
-  batch_size=batch_size)
+    data_dir,
+    validation_split=0.2,
+    subset="validation",
+    seed=123,
+    image_size=(img_height, img_width),
+    batch_size=batch_size)
 
 class_names = train_ds.class_names
 print(class_names)
 
 plt.figure(figsize=(10, 10))
 for images, labels in train_ds.take(1):
-  for i in range(9):
-    ax = plt.subplot(3, 3, i + 1)
-    plt.imshow(images[i].numpy().astype("uint8"))
-    plt.title(class_names[labels[i]])
-    plt.axis("off")
+    for i in range(9):
+        ax = plt.subplot(3, 3, i + 1)
+        plt.imshow(images[i].numpy().astype("uint8"))
+        plt.title(class_names[labels[i]])
+        plt.axis("off")
 
 for image_batch, labels_batch in train_ds:
-  print(image_batch.shape)
-  print(labels_batch.shape)
-  break
-
+    print(image_batch.shape)
+    print(labels_batch.shape)
+    break
 
 AUTOTUNE = tf.data.experimental.AUTOTUNE
 
@@ -76,7 +76,7 @@ train_ds = train_ds.cache().shuffle(1000).prefetch(buffer_size=AUTOTUNE)
 ## Dataset.prefetch() overlaps data preprocessing and model execution while training.
 val_ds = val_ds.cache().prefetch(buffer_size=AUTOTUNE)
 
-normalization_layer = layers.experimental.preprocessing.Rescaling(1./255)
+normalization_layer = layers.experimental.preprocessing.Rescaling(1. / 255)
 
 normalized_ds = train_ds.map(lambda x, y: (normalization_layer(x), y))
 image_batch, labels_batch = next(iter(normalized_ds))
@@ -87,16 +87,16 @@ print(np.min(first_image), np.max(first_image))
 num_classes = 5
 
 model = Sequential([
-  layers.experimental.preprocessing.Rescaling(1./255, input_shape=(img_height, img_width, 3)),
-  layers.Conv2D(16, 3, padding='same', activation='relu'),
-  layers.MaxPooling2D(),
-  layers.Conv2D(32, 3, padding='same', activation='relu'),
-  layers.MaxPooling2D(),
-  layers.Conv2D(64, 3, padding='same', activation='relu'),
-  layers.MaxPooling2D(),
-  layers.Flatten(),
-  layers.Dense(128, activation='relu'),
-  layers.Dense(num_classes)
+    layers.experimental.preprocessing.Rescaling(1. / 255, input_shape=(img_height, img_width, 3)),
+    layers.Conv2D(16, 3, padding='same', activation='relu'),
+    layers.MaxPooling2D(),
+    layers.Conv2D(32, 3, padding='same', activation='relu'),
+    layers.MaxPooling2D(),
+    layers.Conv2D(64, 3, padding='same', activation='relu'),
+    layers.MaxPooling2D(),
+    layers.Flatten(),
+    layers.Dense(128, activation='relu'),
+    layers.Dense(num_classes)
 ])
 
 model.compile(optimizer='adam',
@@ -106,13 +106,12 @@ model.compile(optimizer='adam',
 model.summary()
 
 ## Train the model
-epochs=10
+epochs = 10
 history = model.fit(
-  train_ds,
-  validation_data=val_ds,
-  epochs=epochs
+    train_ds,
+    validation_data=val_ds,
+    epochs=epochs
 )
-
 
 ## Visualize training results
 acc = history.history['accuracy']
@@ -135,4 +134,8 @@ plt.plot(epochs_range, loss, label='Training Loss')
 plt.plot(epochs_range, val_loss, label='Validation Loss')
 plt.legend(loc='upper right')
 plt.title('Training and Validation Loss')
-plt.show()
+# plt.show()
+plt.savefig(str(round(time.time() * 1000)) + '_loss&train.png')
+plt.close()
+
+## 以上的训练代码会出现过拟合
