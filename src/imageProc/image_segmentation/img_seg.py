2da389dbc70bcb5c29d03fdaa9fc1afe703a25bd
wangquanzhou <audi.car@qq.com>
2021-01-24 13:25:09 +0800

    add note

--- src/imageProc/image_segmentation/img_seg.py
+++ src/imageProc/image_segmentation/img_seg.py
@@ -4,6 +4,7 @@ import ssl
 from tensorflow_examples.models.pix2pix import pix2pix
 
 import tensorflow_datasets as tfds
+
 tfds.disable_progress_bar()
 
 from IPython.display import clear_output
@@ -12,31 +13,45 @@ import matplotlib.pyplot as plt
 ssl._create_default_https_context = ssl._create_unverified_context
 
 dataset, info = tfds.load('oxford_iiit_pet:3.*.*', with_info=True)
-def normalize(input_image, input_mask):
-  input_image = tf.cast(input_image, tf.float32) / 255.0
-  input_mask -= 1
-  return input_image, input_mask
 
+
+## 对图像做归一化处理
+## input_image为0-1之间的像素点矩阵   input_mask为-1至0之间的矩阵
+def normalize(input_image, input_mask):
+    input_image = tf.cast(input_image, tf.float32) / 255.0
+    input_mask -= 1
+    return input_image, input_mask
+
+
+## 对原始图像数据以及图像掩码数据  统一图像长宽大小
 @tf.function
 def load_image_train(datapoint):
-  input_image = tf.image.resize(datapoint['image'], (128, 128))
-  input_mask = tf.image.resize(datapoint['segmentation_mask'], (128, 128))
+    input_image = tf.image.resize(datapoint['image'], (128, 128))
+    input_mask = tf.image.resize(datapoint['segmentation_mask'], (128, 128))
 
-  if tf.random.uniform(()) > 0.5:
-    input_image = tf.image.flip_left_right(input_image)
-    input_mask = tf.image.flip_left_right(input_mask)
+    ## tf.random.uniform(()) 返回的随机值服从正太分布
+    ## 因此下面的if逻辑判断  有一半的几率  图像的左右会产生翻转
+    if tf.random.uniform(()) > 0.5:
+        input_image = tf.image.flip_left_right(input_image)
+        input_mask = tf.image.flip_left_right(input_mask)
 
-  input_image, input_mask = normalize(input_image, input_mask)
+    input_image, input_mask = normalize(input_image, input_mask)
 
-  return input_image, input_mask
+    return input_image, input_mask
 
+
+## 测试图像统一调整大小
 def load_image_test(datapoint):
-  input_image = tf.image.resize(datapoint['image'], (128, 128))
-  input_mask = tf.image.resize(datapoint['segmentation_mask'], (128, 128))
+    input_image = tf.image.resize(datapoint['image'], (128, 128))
+    input_mask = tf.image.resize(datapoint['segmentation_mask'], (128, 128))
 
-  input_image, input_mask = normalize(input_image, input_mask)
+    ## 注意：测试集使用的图像是不需要进行翻转的
 
-  return input_image, input_mask
+    ## 归一化图像
+    input_image, input_mask = normalize(input_image, input_mask)
+
+    return input_image, input_mask
+
 
 TRAIN_LENGTH = info.splits['train'].num_examples
 BATCH_SIZE = 64
@@ -50,22 +65,23 @@ train_dataset = train.cache().shuffle(BUFFER_SIZE).batch(BATCH_SIZE).repeat()
 train_dataset = train_dataset.prefetch(buffer_size=tf.data.experimental.AUTOTUNE)
 test_dataset = test.batch(BATCH_SIZE)
 
+
 def display(display_list):
-  plt.figure(figsize=(15, 15))
+    plt.figure(figsize=(15, 15))
 
-  title = ['Input Image', 'True Mask', 'Predicted Mask']
+    title = ['Input Image', 'True Mask', 'Predicted Mask']
 
-  for i in range(len(display_list)):
-    plt.subplot(1, len(display_list), i+1)
-    plt.title(title[i])
-    plt.imshow(tf.keras.preprocessing.image.array_to_img(display_list[i]))
-    plt.axis('off')
-  plt.show()
+    for i in range(len(display_list)):
+        plt.subplot(1, len(display_list), i + 1)
+        plt.title(title[i])
+        plt.imshow(tf.keras.preprocessing.image.array_to_img(display_list[i]))
+        plt.axis('off')
+    plt.show()
+
 
 for image, mask in train.take(1):
-      sample_image, sample_mask = image, mask
+    sample_image, sample_mask = image, mask
 display([sample_image, sample_mask])
-
 
 OUTPUT_CHANNELS = 3
 
@@ -73,11 +89,11 @@ base_model = tf.keras.applications.MobileNetV2(input_shape=[128, 128, 3], includ
 
 # 使用这些层的激活设置
 layer_names = [
-    'block_1_expand_relu',   # 64x64
-    'block_3_expand_relu',   # 32x32
-    'block_6_expand_relu',   # 16x16
+    'block_1_expand_relu',  # 64x64
+    'block_3_expand_relu',  # 32x32
+    'block_6_expand_relu',  # 16x16
     'block_13_expand_relu',  # 8x8
-    'block_16_project',      # 4x4
+    'block_16_project',  # 4x4
 ]
 layers = [base_model.get_layer(name).output for name in layer_names]
 
@@ -90,32 +106,34 @@ up_stack = [
     pix2pix.upsample(512, 3),  # 4x4 -> 8x8
     pix2pix.upsample(256, 3),  # 8x8 -> 16x16
     pix2pix.upsample(128, 3),  # 16x16 -> 32x32
-    pix2pix.upsample(64, 3),   # 32x32 -> 64x64
+    pix2pix.upsample(64, 3),  # 32x32 -> 64x64
 ]
 
+
 def unet_model(output_channels):
-  inputs = tf.keras.layers.Input(shape=[128, 128, 3])
-  x = inputs
+    inputs = tf.keras.layers.Input(shape=[128, 128, 3])
+    x = inputs
 
-  # 在模型中降频取样
-  skips = down_stack(x)
-  x = skips[-1]
-  skips = reversed(skips[:-1])
+    # 在模型中降频取样
+    skips = down_stack(x)
+    x = skips[-1]
+    skips = reversed(skips[:-1])
 
-  # 升频取样然后建立跳跃连接
-  for up, skip in zip(up_stack, skips):
-    x = up(x)
-    concat = tf.keras.layers.Concatenate()
-    x = concat([x, skip])
+    # 升频取样然后建立跳跃连接
+    for up, skip in zip(up_stack, skips):
+        x = up(x)
+        concat = tf.keras.layers.Concatenate()
+        x = concat([x, skip])
 
-  # 这是模型的最后一层
-  last = tf.keras.layers.Conv2DTranspose(
-      output_channels, 3, strides=2,
-      padding='same')  #64x64 -> 128x128
+    # 这是模型的最后一层
+    last = tf.keras.layers.Conv2DTranspose(
+        output_channels, 3, strides=2,
+        padding='same')  # 64x64 -> 128x128
 
-  x = last(x)
+    x = last(x)
 
-  return tf.keras.Model(inputs=inputs, outputs=x)
+    return tf.keras.Model(inputs=inputs, outputs=x)
+
 
 model = unet_model(OUTPUT_CHANNELS)
 model.compile(optimizer='adam',
@@ -124,31 +142,36 @@ model.compile(optimizer='adam',
 
 tf.keras.utils.plot_model(model, show_shapes=True)
 
+
 def create_mask(pred_mask):
-  pred_mask = tf.argmax(pred_mask, axis=-1)
-  pred_mask = pred_mask[..., tf.newaxis]
-  return pred_mask[0]
+    pred_mask = tf.argmax(pred_mask, axis=-1)
+    pred_mask = pred_mask[..., tf.newaxis]
+    return pred_mask[0]
+
 
 def show_predictions(dataset=None, num=1):
-  if dataset:
-    for image, mask in dataset.take(num):
-      pred_mask = model.predict(image)
-      display([image[0], mask[0], create_mask(pred_mask)])
-  else:
-    display([sample_image, sample_mask,
-             create_mask(model.predict(sample_image[tf.newaxis, ...]))])
+    if dataset:
+        for image, mask in dataset.take(num):
+            pred_mask = model.predict(image)
+            display([image[0], mask[0], create_mask(pred_mask)])
+    else:
+        display([sample_image, sample_mask,
+                 create_mask(model.predict(sample_image[tf.newaxis, ...]))])
+
 
 show_predictions()
 
+
 class DisplayCallback(tf.keras.callbacks.Callback):
-  def on_epoch_end(self, epoch, logs=None):
-    clear_output(wait=True)
-    show_predictions()
-    print ('\nSample Prediction after epoch {}\n'.format(epoch+1))
+    def on_epoch_end(self, epoch, logs=None):
+        clear_output(wait=True)
+        show_predictions()
+        print('\nSample Prediction after epoch {}\n'.format(epoch + 1))
+
 
 EPOCHS = 20
 VAL_SUBSPLITS = 5
-VALIDATION_STEPS = info.splits['test'].num_examples//BATCH_SIZE//VAL_SUBSPLITS
+VALIDATION_STEPS = info.splits['test'].num_examples // BATCH_SIZE // VAL_SUBSPLITS
 
 model_history = model.fit(train_dataset, epochs=EPOCHS,
                           steps_per_epoch=STEPS_PER_EPOCH,
